ae4647cb82ebb0f5602490da40d210c362b7fe2a
heyunlong565 <734274314@qq.com>
2014-03-19 21:05:55 +0800

    Update JS.js

--- JS.js
+++ JS.js
@@ -1,3 +1,4 @@
+//返回 N个在[min,max]范围内的不重复的数
 var createRandom = function(){
   var rst =[], arr = [];
   function createNum(min, max, n){
@@ -16,4 +17,14 @@ var createRandom = function(){
   }
   
   return createNum;
+};
+//实现bind
+var bind = function(element, type, handler){
+  if(element.addEventListener){
+    element.addEventListener(type, handler, false);  
+  }else if( element.attachEvent){
+    element.attchEvent("on"+type, handler);
+  }else{
+    element["on"+type] = handler;
+  }
 };
